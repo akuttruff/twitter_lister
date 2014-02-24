79de6890d3418bb3352845ece0fd21af2fff2398
Amie Kuttruff <amie.kuttruff@gmail.com>
2014-02-24 01:28:03 -0800

    updated app.rb

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ require './secret.rb'
 # curl https://twitter.com/mjfreshyfresh/zen-masters/members > handles.html 
 # use regex to extract twitter screennames 
 # (couldn't get regex to work, created array by hand - d'oh!)
-
 baseurl = "https://api.twitter.com"
 path    = "/1.1/users/lookup.json"
 query   = URI.encode_www_form("screen_name" => TWITTER_HANDLES)
@@ -17,38 +16,33 @@ address = URI("#{baseurl}#{path}?#{query}")
 request = Net::HTTP::Get.new(address.request_uri)
 
 # sets up HTTP
-
 http             = Net::HTTP.new(address.host, address.port)
 http.use_ssl     = true
 http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
 # CONSUMER_KEY and ACCESS_TOKEN stored in secret.rb
-
 request.oauth!(http, CONSUMER_KEY, ACCESS_TOKEN)
 http.start
 response = http.request(request)
 
 # creates a JSON representation of the members' info;
 # 'description' will give us bios
-
 if response.code == '200' then
   data = JSON.parse(response.body)
 end
- 
-data.each do |handle|
-	description = handle["description"]
-	puts description
-end
-  	 
- 
-  
-#  words = description.split(" ")
-#  sorted_words = words.sort
-#  puts sorted_words
-#  end
 
-# take JSON array and split into separate string values
+# JSON array -> string -> split strings of words
+bio = data.map { |h| h["description"] }
+bio_string = bio.join
+bio_string.downcase!
+words = bio_string.split(" ")
 
-
+# create hash to store word frequencies
+frequencies = Hash.new(0)
+words.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by {|a, b| b }
+frequencies.reverse!
+frequencies.each { |word, frequency| puts "#{word}:#{frequency}" }
+  	
 # write out data to a static .js file (??)
 # use d3 to illustrate findings
